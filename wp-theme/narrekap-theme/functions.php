0bd077a719c5b113a4ddfb014bbeac8116807bf5
mjaveld <10114152+mjaveld@users.noreply.github.com.>
2024-11-12 14:49:22 +0100

    Created WP theme folder for narrekap-theater-theme

---
+++ wp-theme/narrekap-theme/functions.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Narrekap Theater Theme functions and definitions
+ *
+ * @link https://developer.wordpress.org/themes/basics/theme-functions/
+ *
+ * @package Narrekap Theater Theme
+ * @since Narrekap Theater Theme 1.0
+ */
+
+ 
